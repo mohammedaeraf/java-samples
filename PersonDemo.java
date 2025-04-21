3174ec1fea69d6d5aa7b07f5a39bffb658d1123b
Mohammed Aeraf <aeraf@outlook.com>
2025-04-21 09:38:05 +0530

    OOP - Class and Object

---
+++ PersonDemo.java
@@ -0,0 +1,39 @@
+public class PersonDemo {
+
+    public static void main(String[] args) {
+        int x;
+        Person person1 = new Person("John", 25);
+        person1.displayInfo();
+
+        Person person2 = new Person("Mohammed", 22);
+        person2.displayInfo();
+    }
+
+}
+
+class Person {
+    // 1. Properties/Attributes
+    String name;
+    int age;
+
+    // Constructor (Function)
+
+    // Default Constructor
+    Person() {
+        this.name = "Person X";
+        this.age = 1;
+    }
+
+    Person(String n, int a) {
+        this.name = n;
+        this.age = a;
+    }
+
+    void displayInfo() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Age: " + this.age);
+    }
+
+    // Functions/Methods
+
+}
